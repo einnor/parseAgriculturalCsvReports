b97c2d45d66c250a648a5bc459d9fb1455021014
Ronnie Nyaga <ronnienyaga@gmail.com>
2020-05-20 16:57:28 +0300

    Debugging

--- index.js
+++ index.js
@@ -17,7 +17,19 @@ const header = [
 fs.createReadStream('FAOSTAT_data_5-20-2020.csv')
   .pipe(csv())
   .on('data', (row) => {
-    console.log(row);
+		const key = row.Year;
+		const item = row.Item;
+		const value = row.Value;
+		if (!dataObject[key]) {
+			dataObject[key] = {};
+		}
+		dataObject[key].year = key;
+		dataObject[key].camel = item === 'Camel' ? value: null;
+		dataObject[key].cattle = item === 'Cattle' ? value: null;
+		dataObject[key].chickens = item === 'Chickens' ? value: null;
+		dataObject[key].goats = item === 'Goats' ? value: null;
+		dataObject[key].pigs = item === 'Pigs' ? value: null;
+		dataObject[key].sheep = item === 'Sheep' ? value: null;
   })
   .on('end', () => {
     console.log('CSV file successfully processed');
