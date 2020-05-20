8b73500d2bad1c879c8b65a2cb2438f1ef2c8049
Ronnie Nyaga <ronnienyaga@gmail.com>
2020-05-20 17:49:57 +0300

    Readline

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
 const csv = require('csv-parser');
 const fs = require('fs');
+const readline = require('readline');
+
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
 
 const writeToCsv = require('./writeToCsv');
 
