f108afbf4bb802906819f6ae0598b88b99be39eb
Ronnie Nyaga <ronnienyaga@gmail.com>
2020-05-20 17:24:55 +0300

    Fixed bug

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const writeToCsv = require('./writeToCsv');
 const dataObject = {};
 const header = [
   { id: 'year', title: 'Year' },
-	{ id: 'camel', title: 'Camel' },
+	{ id: 'camels', title: 'Camels' },
   { id: 'cattle', title: 'Cattle' },
 	{ id: 'chickens', title: 'Chickens' },
 	{ id: 'goats', title: 'Goats' },
@@ -20,42 +20,15 @@ fs.createReadStream('FAOSTAT_data_5-20-2020.csv')
 		const key = row.Year;
 		const item = row.Item;
 		const value = row.Value;
-		if (!dataObject[key]) {
+		if (dataObject[key] === undefined) {
 			dataObject[key] = {};
 		}
-		dataObject[key].year = key;
-		dataObject[key].camel = item === 'Camel' ? value: null;
-		dataObject[key].cattle = item === 'Cattle' ? value: null;
-		dataObject[key].chickens = item === 'Chickens' ? value: null;
-		dataObject[key].goats = item === 'Goats' ? value: null;
-		dataObject[key].pigs = item === 'Pigs' ? value: null;
-		dataObject[key].sheep = item === 'Sheep' ? value: null;
+		dataObject[key].year = `Y${key}`;
+		dataObject[key][item.toLowerCase()] = value;
   })
   .on('end', () => {
-    console.log('CSV file successfully processed');
+		console.log('CSV file successfully processed');
+		console.log(dataObject);
+		const data = Object.keys(dataObject).map((key) => dataObject[key]);
+		writeToCsv(header, data);
 	});
-
-const data = Object.keys(dataObject).map((key) => dataObject[key]);
-writeToCsv(header, data);
-
-
-// const data = [
-// 	{
-// 		year: 2000,
-// 		camel: 12,
-// 		cattle: 26,
-// 		chickens: 30,
-// 		goats: 40,
-// 		pigs: 10,
-// 		sheep: 15,
-// 	},
-// 	{
-// 		year: 2001,
-// 		camel: 19,
-// 		cattle: 21,
-// 		chickens: 30,
-// 		goats: 20,
-// 		pigs: 15,
-// 		sheep: 10,
-// 	},
-// ];
