44ab27720d012cf61df2cfc949aaacf44a1a30e5
Ronnie Nyaga <ronnienyaga@gmail.com>
2020-05-20 16:48:36 +0300

    Refactor

--- index.js
+++ index.js
@@ -1,6 +1,19 @@
 const csv = require('csv-parser');
 const fs = require('fs');
 
+const writeToCsv = require('./writeToCsv');
+
+const dataObject = {};
+const header = [
+  { id: 'year', title: 'Year' },
+	{ id: 'camel', title: 'Camel' },
+  { id: 'cattle', title: 'Cattle' },
+	{ id: 'chickens', title: 'Chickens' },
+	{ id: 'goats', title: 'Goats' },
+	{ id: 'pigs', title: 'Pigs' },
+	{ id: 'sheep', title: 'Sheep' },
+]
+
 fs.createReadStream('FAOSTAT_data_5-20-2020.csv')
   .pipe(csv())
   .on('data', (row) => {
@@ -8,4 +21,29 @@ fs.createReadStream('FAOSTAT_data_5-20-2020.csv')
   })
   .on('end', () => {
     console.log('CSV file successfully processed');
-  });
+	});
+
+const data = Object.keys(dataObject).map((key) => dataObject[key]);
+writeToCsv(header, data);
+
+
+// const data = [
+// 	{
+// 		year: 2000,
+// 		camel: 12,
+// 		cattle: 26,
+// 		chickens: 30,
+// 		goats: 40,
+// 		pigs: 10,
+// 		sheep: 15,
+// 	},
+// 	{
+// 		year: 2001,
+// 		camel: 19,
+// 		cattle: 21,
+// 		chickens: 30,
+// 		goats: 20,
+// 		pigs: 15,
+// 		sheep: 10,
+// 	},
+// ];
