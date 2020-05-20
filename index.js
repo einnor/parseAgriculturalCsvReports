1b88c6330d661fde84b7ea2fec0dc5659ff2ebe8
Ronnie Nyaga <ronnienyaga@gmail.com>
2020-05-20 18:08:37 +0300

    Provide path to output file

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const csv = require('csv-parser');
 const fs = require('fs');
 const readline = require('readline');
 
+const writeToCsv = require('./writeToCsv');
+
 const rl = readline.createInterface({
 	input: process.stdin,
 	output: process.stdout
@@ -17,11 +19,42 @@ const titleCase = (title) => {
 
 rl.question('Drag and drop the csv you want to parse here and press ENTER', (path) => {
 	rl.question('Enter the headers, separating them by commas, then press ENTER', (headers) => {
-		console.log('\n\n\nProcessing...\n\n\n');
+		console.log('\n\n\nInitializing...\n\n\n');
 
 		const header = headers.split(',').map((item) => ({
-			id: item.toLowerCase(), title: item.toTe
+			id: item.toLowerCase(),
+			title: titleCase(item),
 		}));
+
+		const dataObject = {};
+		const header = [
+			{ id: 'year', title: 'Year' },
+			{ id: 'camels', title: 'Camels' },
+			{ id: 'cattle', title: 'Cattle' },
+			{ id: 'chickens', title: 'Chickens' },
+			{ id: 'goats', title: 'Goats' },
+			{ id: 'pigs', title: 'Pigs' },
+			{ id: 'sheep', title: 'Sheep' },
+		];
+
+		fs.createReadStream(path)
+			.pipe(csv())
+			.on('data', (row) => {
+				const key = row.Year;
+				const item = row.Item;
+				const value = row.Value;
+				if (dataObject[key] === undefined) {
+					dataObject[key] = {};
+				}
+				dataObject[key].year = `Y${key}`;
+				dataObject[key][item.toLowerCase()] = value;
+			})
+			.on('end', () => {
+				console.log('CSV file successfully processed');
+				console.log(dataObject);
+				const data = Object.keys(dataObject).map((key) => dataObject[key]);
+				writeToCsv(header, data, `[PROCESSED]${path}`);
+			});
 		rl.close();
 	});
 });
@@ -30,37 +63,3 @@ rl.on('close', function() {
 	console.log("\nBYE BYE !!!");
 	process.exit(0);
 });
-
-
-
-const writeToCsv = require('./writeToCsv');
-
-const dataObject = {};
-const header = [
-  { id: 'year', title: 'Year' },
-	{ id: 'camels', title: 'Camels' },
-  { id: 'cattle', title: 'Cattle' },
-	{ id: 'chickens', title: 'Chickens' },
-	{ id: 'goats', title: 'Goats' },
-	{ id: 'pigs', title: 'Pigs' },
-	{ id: 'sheep', title: 'Sheep' },
-]
-
-fs.createReadStream('FAOSTAT_data_5-20-2020.csv')
-  .pipe(csv())
-  .on('data', (row) => {
-		const key = row.Year;
-		const item = row.Item;
-		const value = row.Value;
-		if (dataObject[key] === undefined) {
-			dataObject[key] = {};
-		}
-		dataObject[key].year = `Y${key}`;
-		dataObject[key][item.toLowerCase()] = value;
-  })
-  .on('end', () => {
-		console.log('CSV file successfully processed');
-		console.log(dataObject);
-		const data = Object.keys(dataObject).map((key) => dataObject[key]);
-		writeToCsv(header, data);
-	});
--- writeToCsv.js
+++ writeToCsv.js
@@ -1,13 +1,13 @@
 const createCsvWriter = require('csv-writer').createObjectCsvWriter;
 
-const writeToCsv = (header = [], data = []) => {
+const writeToCsv = (header = [], data = [], outputPath = 'output.csv') => {
 	const csvWriter = createCsvWriter({
-		path: 'output.csv',
+		path: outputPath,
 		header,
 	});
 	csvWriter
 	.writeRecords(data)
-	.then(()=> console.log('The CSV file was written successfully'));
+	.then(()=> console.log('The CSV file was written successfully!'));
 };
 
 module.exports = writeToCsv;
